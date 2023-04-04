f819b4b888396eac9e6bd84afafc9fe3906d3c0a
Sandra Kulibali <sandra.kulibali@gmail.com>
2023-04-04 15:05:32 +0600

    lesson_01 + a little bit of lesson_02

---
+++ stepshop/stepshop/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
+
+
+def products(request):
+    return render(request, 'products.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def product(request):
+    return render(request, 'product.html')
